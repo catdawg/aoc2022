ee4487a373c743201713ecf654dadd043ec6d2ef
Rui Campos <rui.campos@thetoadman.com>
2022-12-09 13:22:38 +0100

    Day08 - treetop tree house

---
+++ day08_treetop_tree_house/src/main.rs
@@ -0,0 +1,120 @@
+use std::env;
+use std::fs;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+
+	let file_path = &args[1];
+
+	let input = fs::read_to_string(file_path)
+		.expect("Should have been able to read the file");
+		
+	let now = std::time::Instant::now();
+
+	let mut line_iter = input.lines().peekable();
+
+	let num_cols = line_iter.peek().unwrap().len();
+
+	let new_line_len = 1; // could be wrong, but it's ok
+	let mut grid: Vec<u8> = Vec::with_capacity(num_cols * (input.len() / (num_cols + new_line_len)));
+
+	for line in input.lines() {
+		for char in line.chars() {
+			grid.push(((char as u32) - ('0' as u32)).try_into().unwrap());
+		}
+	}
+	
+	let get_tree = |row: usize, col: usize| {
+		grid.get(row * num_cols + col).unwrap()
+	};
+
+	let num_rows = grid.len() / num_cols;
+
+	let res = grid.iter().enumerate().fold(0, |acc, (index, tree)| {
+		let col = index % num_cols;
+		let row = index / num_cols;
+
+		if row == 0 || row == (num_rows - 1) || col == 0 || col == (num_cols - 1) {
+			return acc + 1
+		}
+
+		// check from top
+		{
+			let mut success = true;
+
+			for new_row in (0..row).rev() {
+				let next_tree = get_tree(new_row, col);
+
+				if *next_tree >= *tree {
+					success = false;
+					break;
+				}
+			}
+
+			if success {
+				return acc + 1;
+			}
+		}
+
+		// check from bottom
+		{
+			let mut success = true;
+
+			for new_row in (row + 1)..num_rows {
+				let next_tree = get_tree(new_row, col);
+
+				if *next_tree >= *tree {
+					success = false;
+					break;
+				}
+			}
+
+			if success {
+				return acc + 1;
+			}
+		}
+		
+		// check from left
+		{
+			let mut success = true;
+
+			for new_col in (0..col).rev() {
+				let next_tree = get_tree(row, new_col);
+
+				if *next_tree >= *tree {
+					success = false;
+					break;
+				}
+			}
+
+			if success {
+				return acc + 1;
+			}
+		}
+
+		// check from right
+		{
+			let mut success = true;
+
+			for new_col in (col + 1)..num_cols {
+				let next_tree = get_tree(row, new_col);
+
+				if *next_tree >= *tree {
+					success = false;
+					break;
+				}
+			}
+
+			if success {
+				return acc + 1;
+			}
+		}
+
+		acc
+	});
+
+	let elapsed = now.elapsed();
+	println!("Elapsed: {:.2?}", elapsed);
+
+	println!("Result: {res}");
+}
